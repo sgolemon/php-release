612e39968c232e87bc6ad9cdde890ae5207aa80a
Sara Golemon <pollita@php.net>
2017-06-20 14:52:23 -0400

    Sign tarballs first, then tag
    The tarball signing is a direct invocation of gpg
    and allows gpg-agent to ask for our key on the terminal.
    The git tag signing is in a child process and can't ask.
    
    By signing the tarballs first, we guarantee gpg-agent
    has a non-expired key in memory.

--- sign.sh
+++ sign.sh
@@ -7,8 +7,8 @@ TAG_COMMIT=${3:?"Missing tag commit"}
 GPG_KEY=${4:?"Missing GPG fingerprint"}
 GPG_USER=${5:?"Missing GPG user"}
 
-git tag -u "${GPG_KEY}" "php-${RELEASE_VERSION}" -m "Tag for php ${RELEASE_VERSION}" "$TAG_COMMIT"
 for COMP in gz bz2 xz; do
 	gpg -u "${GPG_USER}" --armor --detach-sign "${PHP_SRC}/php-${RELEASE_VERSION}.tar.$COMP"
 done
+git tag -u "${GPG_KEY}" "php-${RELEASE_VERSION}" -m "Tag for php ${RELEASE_VERSION}" "$TAG_COMMIT"
 
