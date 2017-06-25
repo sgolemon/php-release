28b3374e891b5c05496fedb2c3e638f3b0ad8ec7
Sara Golemon <pollita@php.net>
2017-06-25 16:27:42 -0400

    Make sure GPG_TTY is set

--- sign.sh
+++ sign.sh
@@ -8,7 +8,7 @@ GPG_KEY=${4:?"Missing GPG fingerprint"}
 GPG_USER=${5:?"Missing GPG user"}
 
 for COMP in gz bz2 xz; do
-	gpg -u "${GPG_USER}" --armor --detach-sign "${PHP_SRC}/php-${RELEASE_VERSION}.tar.$COMP"
+	GPG_TTY=${GPG_TTY:-$(tty)} gpg -u "${GPG_USER}" --armor --detach-sign "${PHP_SRC}/php-${RELEASE_VERSION}.tar.$COMP"
 done
-git tag -u "${GPG_KEY}" "php-${RELEASE_VERSION}" -m "Tag for php ${RELEASE_VERSION}" "$TAG_COMMIT"
+GPG_TTY=${GPG_TTY:-$(tty)} git tag -u "${GPG_KEY}" "php-${RELEASE_VERSION}" -m "Tag for php ${RELEASE_VERSION}" "$TAG_COMMIT"
 
