7032af386a5cf3c0ad958d40d7772c5710fec365
Sara Golemon <pollita@php.net>
2017-07-06 12:47:05 -0400

    Remove md5 from manifest

--- manifest.sh
+++ manifest.sh
@@ -5,8 +5,6 @@ BASENAME=${1:?"Mising basename of tarfile"}
 
 for COMP in gz bz2 xz; do
 	echo "$BASENAME.$COMP"
-	echo -n "MD5 hash: "
-	md5sum "$BASENAME.$COMP" | cut -d ' ' -f 1
 	echo -n "SHA256 hash: "
 	sha256sum "$BASENAME.$COMP" | cut -d ' ' -f 1
 	if [ -f "$BASENAME.$COMP.asc" ]; then
